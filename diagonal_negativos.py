aefa96ed6d8d43b50c7cd32f7328b41521251800
Rhuan Santos <rhuankito@hotmail.com>
2024-12-28 14:27:52 -0300

    Add files via upload

---
+++ diagonal_negativos.py
@@ -0,0 +1,21 @@
+qnt: int
+
+N = int(input("Qual a ordem da matriz? "))
+
+mat: [[int]] = [[0 for x in range (N)] for x in range (N)]
+
+for i in range(0, N):
+    for j in range(0, N):
+        mat[i][j] = int(input(f"Elemento [{i},{j}]: "))
+
+print("DIAGONAL PRINCIPAL:")
+for i in range(0,N):
+    print(f"{mat[i][i]}  ", end = "")
+
+qnt = 0
+for i in range(0, N):
+    for j in range(0, N):
+        if mat[i][j] < 0:
+            qnt = qnt + 1
+print()
+print(f"QUANTIDADE DE NEGATIVOS = {qnt}")
